99d72aae2d78bc787f816950dd3090720ee6e9d5
untilthecore <814898862@qq.com>
2021-01-12 14:53:05 +0800

    完成vue倒计时组件

---
+++ vueCountDown/src/main.js
@@ -0,0 +1,49 @@
+new Vue({
+  el: '#app',
+  data: {
+    // svg 大小
+    size: 120,
+    // 笔画大小
+    stroke: 10,
+    // 百分比
+    percentage: 100,
+    timer: null,
+    seconds: 5,
+    isAnimate: false
+  },
+  computed: {
+    radius() {
+      return (this.size - this.stroke) / 2
+    },
+    circleOffset() {
+      return this.size / 2
+    },
+    circumference() {
+      return this.radius * 2 * Math.PI
+    },
+    progress() {
+      return this.circumference - this.circumference * this.percentage / 100
+    },
+    countDown() {
+      // 保持向上取整
+      return Math.ceil(
+        this.seconds * this.percentage / 100
+      )
+    }
+  },
+  methods: {
+    animate() {
+      if (!this.isAnimate) {
+        this.isAnimate = true
+        this.timer = window.setInterval(() => {
+          // 控制动画的顺滑度
+          this.percentage -= 1 / 10
+          if (this.percentage <= 0) {
+            clearInterval(this.timer)
+            this.percentage = 100
+          }
+        }, this.seconds * 1000 / 100 / 10)
+      }
+    }
+  }
+})
